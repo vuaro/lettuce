844437f55afc2a946aef578ae2eff2db6306e796
mike <mike@mike-host.(none)>
2013-04-18 19:33:04 +0400

    smtp mail queue plugin implemented

--- lettuce/django/server.py
+++ lettuce/django/server.py
@@ -14,8 +14,13 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import asyncore
+from email import message_from_string
 import os
+from smtpd import SMTPServer
+import smtpd
 import sys
+import threading
 import time
 import socket
 import httplib
